034ec290251ae1fb173f1f4920e741a76e85145e
orysegev89 <orysegev9@gmail.com>
2014-11-05 00:31:45 +0200

    Initial commit

---
+++ src/com/mta/javacourse/MtajavacourseServlet.java
@@ -0,0 +1,13 @@
+package com.mta.javacourse;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class MtajavacourseServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
