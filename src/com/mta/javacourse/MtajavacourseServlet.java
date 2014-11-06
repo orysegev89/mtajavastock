ac6ca9976e65916fe65ebcb95549bc9e1f28d1dd
orysegev89 <orysegev9@gmail.com>
2014-11-06 23:28:02 +0200

    Task 2 java course

--- src/com/mta/javacourse/MtajavacourseServlet.java
+++ src/com/mta/javacourse/MtajavacourseServlet.java
@@ -7,7 +7,17 @@ import javax.servlet.http.*;
 public class MtajavacourseServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.setContentType("text/html");
+		
+		int num1,num2,num3,result;
+	
+		num1 = 4;
+		num2 = 3;
+		num3 = 7;
+		result = (num1 + num2) * num3;
+		
+		String resultStr = new String("<h1>The result of ( "+"" +num1+" + "+num2+" ) * "+num3+" = "+result+"</h1>");
+		
+		resp.getWriter().println(resultStr);
 	}
 }
