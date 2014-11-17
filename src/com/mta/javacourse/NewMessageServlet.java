decd0161fdee9a22ff88f7fc02ddd516c41c0802
orysegev89 <orysegev9@gmail.com>
2014-11-17 08:28:29 +0200

    "hello ory segev" instead "hello app engine"

---
+++ src/com/mta/javacourse/NewMessageServlet.java
@@ -0,0 +1,16 @@
+package com.mta.javacourse;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class NewMessageServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/html");
+	}
+
+}
